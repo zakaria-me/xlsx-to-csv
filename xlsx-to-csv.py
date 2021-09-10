2dd1c1b16fbb7e3bec42d5325516598c01b6f614
Zakaria MELLAH <zakaria.mellah.pro@gmail.com>
2021-09-10 11:45:59 +0200

    add DROP statement at the beginning of the sql_file

--- xlsx-to-csv.py
+++ xlsx-to-csv.py
@@ -55,6 +55,7 @@ def csv_to_sql(csv_files_name :list, directory :str):
       print("Ecriture de " + sql_file_name_to_create + " terminée")
 
 def edit_sql_file(path_to_sql_file :str):
+  add_drop_statement_to_sql_file(path_to_sql_file)
   replace_double_quotes(path_to_sql_file)
   append_copy_statement_to_sql_file(path_to_sql_file)
 
@@ -67,10 +68,21 @@ def replace_double_quotes(path_to_sql_file :str):
   sql_file_write.write(data)
   sql_file_write.close()
 
-def append_copy_statement_to_sql_file(path_to_sql_file):
+def append_copy_statement_to_sql_file(path_to_sql_file :str):
   sql_file_append = open(path_to_sql_file, mode="a")
-  print("-- NE PAS DECOMMENTER \n --\COPY 'nom_du_schema.nom_de_la_table' FROM 'chemin_d_acces_au_fichier_csv' WITH(FORMAT CSV, HEADER True, DELIMITER ';', ENCODING 'UTF-8')", file=sql_file_append) 
+  print("-- NE PAS DECOMMENTER \n -- \COPY 'nom_du_schema.nom_de_la_table' FROM 'chemin_d_acces_au_fichier_csv' WITH(FORMAT CSV, HEADER True, DELIMITER ';', ENCODING 'UTF-8')", file=sql_file_append) 
   sql_file_append.close()
+
+def add_drop_statement_to_sql_file(path_to_sql_file :str):
+  sql_file_read = open(path_to_sql_file, mode="r")
+  data = sql_file_read.read()
+  # the + operator is not the most efficient, maybe try something else?
+  data = "DROP TABLE IF EXISTS 'nom_du_schema.nom_de_la_table'; \n" + data
+  sql_file_read.close()
+  sql_file_write = open(path_to_sql_file, mode="w")
+  sql_file_write.write(data)
+  sql_file_write.close()
+  pass
 
 def create_destination_directories(directory):
   sql_directory = get_sql_dir_path(directory) 
