05ef584bd0bcd20820f410b976d70446c852d1d5
Zakaria MELLAH <zakaria.mellah.pro@gmail.com>
2021-09-16 14:31:16 +0200

    get current directory via user input

--- xlsx-to-csv.py
+++ xlsx-to-csv.py
@@ -200,11 +200,38 @@ def is_interactive():
     print("--- Lancement du programme en mode indépendant ---\n")
     default_settings()
 
+def input_dir_not_valid(directory):
+  if directory == "":
+    return 1
+  try: 
+    # Si le repertoire ne contient pas fichiers_xlsx
+    for dir in os.listdir(directory):
+      if dir == "fichiers_xlsx":
+        return 0
+    return 3
+  except:
+  # Si le repertoire n'existe pas
+    return 2
+
+def get_current_directory():
+  message = "Quel est le chemin amenant au répertoire d'où vous lancez le programme ? "
+  directory = input(message)
+  validity_of_dir_name = input_dir_not_valid(directory)
+  while validity_of_dir_name > 0:
+    if validity_of_dir_name == 1: 
+      directory = input("Le chemin que vous avez spécifié n'est pas valide. Retentez. " + message)
+    elif validity_of_dir_name == 2:
+      directory = input("Le répertoire que vous avez spécifié n'existe pas. Retentez. " + message)
+    elif validity_of_dir_name == 3:
+      directory = input("Ce répertoire ne contient pas le fichier_xlsx! Retentez. " + message)
+    validity_of_dir_name = input_dir_not_valid(directory)
+  return directory
+
 def main():
+  # Prendre en mémoire le chemin vers le répertoire courant
+  directory = get_current_directory() 
   # Ask for interactive or not ?
   is_interactive()
-  # Prendre en mémoire le chemin vers le répertoire courant
-  directory = os.getcwd()
   # Créer les dossiers où seront stockés les fichiers .csv et .sql
   create_destination_directories(directory)
   # Exporter les fichiers xlsx en csv
