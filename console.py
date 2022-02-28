dc6cb2b4765fee41238b6483dc182031383dc0b7
Zakaria MELLAH <zakaria.mellah.pro@gmail.com>
2022-02-28 17:17:42 +0100

    add console module from rich

---
+++ console.py
@@ -0,0 +1,6 @@
+from rich.console import Console
+from rich.style import Style
+from rich.markdown import Markdown
+
+base_style = Style(color="White", bgcolor=None, bold=True)
+console = Console(style=base_style)
