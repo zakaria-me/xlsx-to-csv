8916f0d901eaecaad799c38b86e702f79e7543e3
Zakaria MELLAH <zakaria.mellah.pro@gmail.com>
2022-06-29 11:56:05 +0200

    updated deployment script

--- pyinstaller-script.sh
+++ pyinstaller-script.sh
@@ -1,1 +1,4 @@
-pyinstaller main.py -F --clean -n ../exec/convertisseur
+cd ../exec
+pyinstaller -F --clean -n convertisseur ../src/main.py
+cp dist/convertisseur.exe R:/Traitements/263\ -\ Outil\ creation\ fichiers\ CSV\ en\ UTF8\ et\ requete\ SQL/xlsx_to_csv/exec/dist
+cd -
